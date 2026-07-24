82a0649b6d913e9c54b7031c6b02f60b2c5f8e2c
Luana Mendonça <luana.silva.130@ufrn.edu.br>
2026-07-24 14:01:02 -0300

    Corrigindo erro

--- app/Config/core.php
+++ app/Config/core.php
@@ -233,12 +233,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'LuanaBlogCakePHP2_2026_Seguranca_7f9K2mP4xQ8z');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '92746183502719465830472196583');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
