9424a128e5b039d84e7fd9ca4f8df3041b2e16b2
Carmilla <luana.silva.130@ufrn.edu.br>
2026-07-23 23:31:51 -0300

    Versionando o banco de dados

---
+++ app/Config/database.php
@@ -0,0 +1,30 @@
+<?php
+
+class DATABASE_CONFIG
+{
+	public $default = array(
+		'datasource' => 'Database/Postgres',
+		'persistent' => false,
+		'host' => 'db',
+		'login' => 'blog_user',
+		'password' => 'blog_senha',
+		'database' => 'blog_cake2',
+		'port' => '5432',
+		'schema' => 'public',
+		'prefix' => '',
+		'encoding' => 'utf8'
+	);
+
+	public $test = array(
+		'datasource' => 'Database/Postgres',
+		'persistent' => false,
+		'host' => 'db',
+		'login' => 'blog_user',
+		'password' => 'blog_senha',
+		'database' => 'blog_cake2',
+		'port' => '5432',
+		'schema' => 'public',
+		'prefix' => '',
+		'encoding' => 'utf8'
+	);
+}
