9ca28caa80a59810e74047c911b15078dc6f4b98
Carmilla <luana.silva.130@ufrn.edu.br>
2026-07-23 23:07:06 -0300

    controller feitooooowwwwwwwwwwwwwwwwwww

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -1,34 +1,60 @@
 <?php
-/**
- * Application level Controller
- *
- * This file is application-wide controller file. You can put all
- * application-wide controller-related methods here.
- *
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
- * @package       app.Controller
- * @since         CakePHP(tm) v 0.2.9
- * @license       https://opensource.org/licenses/mit-license.php MIT License
- */
 
 App::uses('Controller', 'Controller');
 
-/**
- * Application Controller
- *
- * Add your application-wide methods in the class below, your controllers
- * will inherit them.
- *
- * @package		app.Controller
- * @link		https://book.cakephp.org/2.0/en/controllers.html#the-app-controller
- */
-class AppController extends Controller {
+class AppController extends Controller
+{
+	public $components = array(
+		'Session',
+
+		'Auth' => array(
+			'loginAction' => array(
+				'controller' => 'users',
+				'action' => 'login'
+			),
+
+			'loginRedirect' => array(
+				'controller' => 'posts',
+				'action' => 'index'
+			),
+
+			'logoutRedirect' => array(
+				'controller' => 'users',
+				'action' => 'login'
+			),
+
+			'authError' => 'Você não tem permissão para acessar esta página.',
+
+			'authenticate' => array(
+				'Form' => array(
+					'fields' => array(
+						'username' => 'username',
+						'password' => 'password'
+					)
+				)
+			),
+
+			'authorize' => array('Controller')
+		)
+	);
+
+	public $helpers = array(
+		'Html',
+		'Form',
+		'Session'
+	);
+
+	public function beforeFilter()
+	{
+		parent::beforeFilter();
+
+		$this->Auth->allow('display');
+
+		$this->set('authUser', $this->Auth->user());
+	}
+
+	public function isAuthorized($user)
+	{
+		return !empty($user);
+	}
 }
